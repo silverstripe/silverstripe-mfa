6366c791945a8c8e889997baf2429754edbefa56
Simon Erkelens <simonerkelens@silverstripe.com>
2018-02-17 16:32:20 +1300

    Add test

--- tests/unit/BackupCodeTest.php
+++ tests/unit/BackupCodeTest.php
@@ -6,6 +6,7 @@ namespace Firesphere\BootstrapMFA\Tests;
 use Firesphere\BootstrapMFA\Models\BackupCode;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Security\Member;
+use SilverStripe\Security\Security;
 
 class BackupCodeTest extends SapphireTest
 {
@@ -17,6 +18,18 @@ class BackupCodeTest extends SapphireTest
         $member = $this->objFromFixture(Member::class, 'member1');
 
         BackupCode::sendWarningEmail($member);
+
+        $this->assertEmailSent($member->Email);
+    }
+
+    public function testWarningMailNotSameUser()
+    {
+        $admin = $this->objFromFixture(Member::class, 'member2');
+        Security::setCurrentUser($admin);
+
+        $member = $this->objFromFixture(Member::class, 'member1');
+
+        BackupCode::generateTokensForMember($member);
 
         $this->assertEmailSent($member->Email);
     }
