f8eff9e65178c35c033610b9a9c0c39dbf3755d0
Simon Erkelens <simonerkelens@silverstripe.com>
2018-02-17 14:52:20 +1300

    Test MemberExtension

---
+++ tests/unit/MemberExtensionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Firesphere\BootstrapMFA\Tests;
+
+use Firesphere\BootstrapMFA\Models\BackupCode;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Security\Member;
+
+class MemberExtensionTest extends SapphireTest
+{
+    protected static $fixture_file = '../fixtures/member.yml';
+
+    public function testMemberCodesExpired()
+    {
+        /** @var Member $member */
+        $member = $this->objFromFixture(Member::class, 'member1');
+
+        $member->updateMFA = true;
+        $member->write();
+        
+        /** @var DataList|BackupCode $codes */
+        $codes = $member->BackupCodes();
+
+        $member->updateMFA = true;
+        $member->write();
+
+        foreach ($codes as $code) {
+            /** @var BackupCode $backup */
+            $backup = BackupCode::get()->byID($code->ID);
+            $this->assertNull($backup);
+        }
+    }
+
+}
