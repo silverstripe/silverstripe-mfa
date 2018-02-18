fca2b56298fb5a1db25a00a9efecf71bdc3ed4c1
Simon Erkelens <simonerkelens@silverstripe.com>
2018-02-18 14:43:59 +1300

    Properly instantiate the form

--- tests/mock/MockMFAHandler.php
+++ tests/mock/MockMFAHandler.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace Firesphere\BootstrapMFA\Tests\Mock;
 
+use Firesphere\BootstrapMFA\Authenticators\BootstrapMFAAuthenticator;
+use Firesphere\BootstrapMFA\Forms\MFALoginForm;
 use Firesphere\BootstrapMFA\Handlers\MFALoginHandler;
+use SilverStripe\Control\Controller;
 
 class MockMFAHandler extends MFALoginHandler
 {
 
     public function MFAForm()
     {
-        return MFALoginForm::create();
+        return MFALoginForm::create(Controller::curr(), BootstrapMFAAuthenticator::class, __FUNCTION__);
     }
 }
