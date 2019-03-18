0bee2a001c524d590c088465034ce24d8f69f5c6
Guy Marriott <guy@scopey.co.nz>
2019-03-18 15:42:58 +1300

    Fixing tests and updating version of react-injector library

--- tests/php/BackupCode/RegisterHandlerTest.php
+++ tests/php/BackupCode/RegisterHandlerTest.php
@@ -8,15 +8,21 @@ use SilverStripe\Core\Config\Config;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\MFA\BackupCode\Method;
 use SilverStripe\MFA\BackupCode\RegisterHandler;
+use SilverStripe\MFA\Extension\MemberExtension;
+use SilverStripe\MFA\Service\MethodRegistry;
 use SilverStripe\MFA\Store\StoreInterface;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\Security;
 
 class RegisterHandlerTest extends SapphireTest
 {
+    protected static $fixture_file = 'LoginHandlerTest.yml';
+
     public function testStartReturnsPlainTextCodes()
     {
         /** @var StoreInterface|PHPUnit_Framework_MockObject_MockObject $store */
         $store = $this->createMock(StoreInterface::class);
-        $store->expects($this->once())->method('setState');
+        $store->expects($this->exactly(2))->method('getMember')->willReturn(Security::getCurrentUser());
 
         $handler = new RegisterHandler();
 
@@ -34,7 +40,7 @@ class RegisterHandlerTest extends SapphireTest
 
         /** @var StoreInterface|PHPUnit_Framework_MockObject_MockObject $store */
         $store = $this->createMock(StoreInterface::class);
-        $store->expects($this->once())->method('setState');
+        $store->expects($this->exactly(2))->method('getMember')->willReturn(Security::getCurrentUser());
 
         $handler = new RegisterHandler();
 
@@ -52,35 +58,49 @@ class RegisterHandlerTest extends SapphireTest
         }
     }
 
-    public function testStartStoresHashesOfBackupCodes()
+    public function testStartStoresHashesOfBackupCodesOnMember()
     {
         /** @var StoreInterface|PHPUnit_Framework_MockObject_MockObject $store */
-        $storedCodes = [];
         $store = $this->createMock(StoreInterface::class);
-        $store->expects($this->once())->method('setState')->willReturnCallback(function ($codes) use (&$storedCodes) {
-            $storedCodes = $codes;
-            return true;
-        });
+
+        /** @var Member|MemberExtension $member */
+        $member = $this->objFromFixture(Member::class, 'guy');
+        $store->expects($this->exactly(2))->method('getMember')->willReturn($member);
 
         $handler = new RegisterHandler();
 
+        // Kill polluted test state :(
+        foreach ($member->RegisteredMFAMethods() as $method) {
+            $method->delete();
+        }
+
+        // Ensure the "current" user has no existing backup codes
+        $registeredMethod = MethodRegistry::singleton()->getRegisteredMethodFromMember($member, 'backup-codes');
+        $this->assertNull($registeredMethod, 'No backup codes are stored yet');
+
+        // Generate the codes and assert and store what's given back to the UI
         $props = $handler->start($store);
-
         $this->assertArrayHasKey('codes', $props, 'Codes are returned from the start method');
-
         $codes = $props['codes'];
 
-        $this->assertCount(count($codes), $storedCodes, 'Stored code count matches that of those returned by start');
+        // Check the registered methods on the member as the start method should have saved new backup codes
+        $registeredMethod = MethodRegistry::singleton()->getRegisteredMethodFromMember($member, 'backup-codes');
+        $this->assertTrue($registeredMethod->isInDB(), 'Backup codes are stored');
+        $this->assertJson($registeredMethod->Data, 'Backup codes are stored as valid JSON');
+
+        // Parse the stored codes on the member to compare with what was given to the UI
+        $storedCodes = json_decode($registeredMethod->Data, true);
+
+        $this->assertCount(count($codes), $storedCodes, 'Stored code count matches that of those returned to the UI');
 
         foreach ($codes as $code) {
-            $this->assertNotContains($code, $storedCodes, 'Code return to UI is not stored in plaintext');
+            $this->assertNotContains($code, $storedCodes, 'Codes returned to UI are plaintext (different from stored)');
         }
     }
 
-    public function testRegisterReturnsStoredState()
+    public function testRegisterReturnsNothing()
     {
         $store = $this->createMock(StoreInterface::class);
-        $store->expects($this->once())->method('getState')->willReturn('something');
 
         /** @var HTTPRequest|PHPUnit_Framework_MockObject_MockObject $request */
         $request = $this->createMock(HTTPRequest::class);
@@ -88,6 +108,6 @@ class RegisterHandlerTest extends SapphireTest
         $handler = new RegisterHandler();
         $result = $handler->register($request, $store);
 
-        $this->assertSame('something', $result);
+        $this->assertEmpty($result);
     }
 }
