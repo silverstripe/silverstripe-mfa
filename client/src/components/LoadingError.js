68a83232e3e6e00f78de3827641cad02e9bf0a0f
Robbie Averill <robbie@silverstripe.com>
2019-06-26 15:41:12 +1200

    Move jsdoc to PropTypes

--- client/src/components/LoadingError.js
+++ client/src/components/LoadingError.js
@@ -1,17 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CircleWarning from 'components/Icons/CircleWarning';
 
 /**
  * Renders an error screen, used for when a schema fetch fails in the
  * MFA app, or if a method is unavailable to be used but is already
  * selected in the verification process.
- *
- * @param {string} title
- * @param {string} message
- * @param {mixed} controls
- * @returns {HTMLElement}
  */
-export default ({ title, message, controls }) => (
+const LoadingError = ({ title, message, controls }) => (
   <div className="mfa-method mfa-method--unavailable">
     <div className="mfa-method-icon mfa-method-icon--unavailable">
       <CircleWarning size="80px" />
@@ -27,3 +23,11 @@ export default ({ title, message, controls }) => (
     </div>
   </div>
 );
+
+LoadingError.propTypes = {
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  controls: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+};
+
+export default LoadingError;
