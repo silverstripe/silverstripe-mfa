855004eca2052bfe9ad55f37772261d82a10f795
Robbie Averill <robbie@silverstripe.com>
2019-08-09 14:33:49 +1200

    Add functional test for MFA report

---
+++ tests/php/Report/EnabledMembersFunctionalTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SilverStripe\MFA\Tests\Report;
+
+use SilverStripe\Dev\FunctionalTest;
+use SilverStripe\MFA\Report\EnabledMembers;
+
+class EnabledMembersFunctionalTest extends FunctionalTest
+{
+    protected static $fixture_file = 'EnabledMembersTest.yml';
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->logInWithPermission();
+    }
+
+    public function testReportHasMemberNames()
+    {
+        $result = (string) $this->get(EnabledMembers::create()->getLink())->getBody();
+
+        $this->assertContains('Eleanor', $result);
+        $this->assertContains('Ditor', $result);
+        $this->assertContains('Michelle', $result);
+        $this->assertContains('Fa', $result);
+        $this->assertContains('Ursula', $result);
+        $this->assertContains('Ser', $result);
+    }
+
+    public function testReportHasRegisteredMethods()
+    {
+        $result = (string) $this->get(EnabledMembers::create()->getLink())->getBody();
+
+        $this->assertContains('Math problem, Null', $result);
+    }
+
+    public function testFilterReportByMemberName()
+    {
+        $this->get(EnabledMembers::create()->getLink());
+        $response = $this->submitForm('Form_EditForm', 'action_updatereport', [
+            'filters[Member]' => 'Michelle',
+        ]);
+        $result = (string) $response->getBody();
+
+        $this->assertContains('mfa@example.com', $result);
+        $this->assertNotContains('admin@example.com', $result);
+    }
+
+    public function testFilterReportBySkippedRegistration()
+    {
+        $this->get(EnabledMembers::create()->getLink());
+        $response = $this->submitForm('Form_EditForm', 'action_updatereport', [
+            'filters[Skipped]' => 'yes',
+        ]);
+        $result = (string) $response->getBody();
+
+        $this->assertContains('user@example.com', $result);
+        $this->assertNotContains('admin@example.com', $result);
+    }
+}
