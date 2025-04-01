e881e8e8f5a98ab741b6cea85e6eb67a52945276
Guy Sartorelli <guy.sartorelli@gmail.com>
2025-04-02 11:45:45 +1300

    API Deprecate API which was removed or changed in CMS 6

--- src/Extension/RequirementsExtension.php
+++ src/Extension/RequirementsExtension.php
@@ -4,6 +4,7 @@ namespace SilverStripe\MFA\Extension;
 
 use SilverStripe\Admin\LeftAndMain;
 use SilverStripe\Core\Extension;
+use SilverStripe\Dev\Deprecation;
 use SilverStripe\MFA\RequestHandler\BaseHandlerTrait;
 
 /**
@@ -15,9 +16,11 @@ class RequirementsExtension extends Extension
 
     /**
      * @see LeftAndMain::init()
+     * @deprecated 5.4.0 Will be renamed to onInit()
      */
     public function init()
     {
+        Deprecation::noticeWithNoReplacment('5.4.0', 'Will be renamed to onInit()');
         // As requirements for this module are dynamic - plugin methods apply their own requirements - we need to
         // include these requirements at run-time (opposed to using $extra_requirements_*)
         $this->applyRequirements(false);
