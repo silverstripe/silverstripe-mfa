85a489163295d31d216845f59db8b97650cb92fb
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2025-05-01 11:26:05 +1200

    ENH Make it clear deprecations are removed in future major (#605)

--- src/Controller/AdminRegistrationController.php
+++ src/Controller/AdminRegistrationController.php
@@ -33,7 +33,7 @@ class AdminRegistrationController extends LeftAndMain
     private static $url_segment = 'mfa';
 
     /**
-     * @deprecated 5.4.0 Will be removed without equivalent functionality to replace it
+     * @deprecated 5.4.0 Will be removed without equivalent functionality to replace it in a future major release
      */
     private static $ignore_menuitem = true;
 
